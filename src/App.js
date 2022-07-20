2b742bedd340f74913240939014235d9c3b9b52d
Shravan PM <97494889+shravanpm@users.noreply.github.com>
2022-07-20 11:20:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-// import { Footer } from './Components/Footer';
-// import { Login } from './Pages/Authentication/Login';
+
 
 function App() {
   return (
     <div className="App">
-      {/* Freshly Clone */}
-      {/* <Login/> */}
-      {/* <Footer/> */}
+  
     </div>
   );
 }
