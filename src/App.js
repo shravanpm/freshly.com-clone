ae1c75d573d135d9ad328502a3cbbd5fb6d01306
shravanpm <shravan.pm@yahoo.com>
2022-07-21 09:40:46 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import Cart from "./Pages/Cart/Cart";
 
 function App() {
-  return (
-    <div className="App">
-      <Cart />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,1 @@
-// NOTE: use this store variable to create a store.
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import { reducer as auth } from "./AuthReducer/reducer";
-import { reducer as appReducer } from "./AppReducer/reducer";
 
-const rootReducer = combineReducers({ auth: auth, appReducer: appReducer });
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
