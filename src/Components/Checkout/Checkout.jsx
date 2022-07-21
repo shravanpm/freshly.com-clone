be3880aa62ba38fe3d5d1cc3020a16fc1b30ace6
Abhishek Giri <70642966+abhishekgiri-cmd@users.noreply.github.com>
2022-07-21 21:40:03 +0530

    code modified

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,90 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Checkout.css";
-import { Input, Stack, Select, Checkbox, Button } from "@chakra-ui/react";
-
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Select,
+  Stack,
+  // InputLeftAddon,
+  Checkbox,
+  //  CheckboxGroup
+} from "@chakra-ui/react";
 export const Checkout = () => {
-  return (
-    <div className="main">
-      <div className="container">
-        <div className="div1">
-          Hello div 1
-          <Stack>
-            <div className="input">
-              <div>
-                {/* <label id="one">First Name</label> */}
-                <h1>First Name</h1>
-                <Input focusBorderColor="blue.400" />
-              </div>
-              <div>
-                <label htmlFor="input">Last Name</label>
-                <Input focusBorderColor="blue.400" />
-              </div>
-            </div>
-          </Stack>
-          <div>
-            <label htmlFor="input">Address</label>
-            <Input placeholder="Address" size="md" />
-          </div>
-          <div>
-            <label htmlFor="input">Address Line 2 ( optional)</label>
-            <Input placeholder="Address" size="md" />
-          </div>
-          <div className="container2">
-            <div>
-              <label htmlFor="">City</label>
-              <Input focusBorderColor="blue.400" />
-            </div>
-            <div>
-              <label htmlFor="">State</label>
-              <Select placeholder="Bihar">
-                <option value="option1">Bihar</option>
-                <option value="option2">Andhra Pradesh</option>
-                <option value="option3">Karnataka</option>
-                <option value="option3">Madhya Pradesh</option>
-                <option value="option3">Asam</option>
-                <option value="option3">Delhi</option>
-                <option value="option3">Chhatisgarh</option>
-              </Select>
-            </div>
-            <div>
-              <label htmlFor="">Zip</label>
-              <Input focusBorderColor="blue.400" />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="">Phone</label>
-            <Input focusBorderColor="blue.400" />
-          </div>
-          <div>
-            <label htmlFor=""></label>
-            <Checkbox defaultChecked>
-              Receive SMS text message updates on my orders
-            </Checkbox>
-          </div>
-          <div>
-            <label htmlFor="">
-              Email (We require your email to send you receipts and order
-              updates)
-            </label>
-            <Input focusBorderColor="blue.400" />
-          </div>
-          <hr />
-          <div>
-            <div className="total">
-              <div>
-                <h2>Total</h2>
-              </div>
-              <div>
-                <h2>$72.80</h2>
-              </div>
-            </div>
-            <Button colorScheme="blue" className="btn">
-              Next
-            </Button>
-          </div>
-        </div>
+  const [input, setInput] = useState("");
 
-        <div className="div2"> hello div 2</div>
+  const handleInputChange = (e) => setInput(e.target.value);
+
+  const isError = input === "";
+  return (
+    <div className="Main">
+      <div className="Body">
+        <div className="div1">
+          <FormControl isInvalid={isError}>
+            <div className="name">
+              <div>
+                <FormLabel>First Name</FormLabel>
+                <Input
+                  type="email"
+                  value={input}
+                  onChange={handleInputChange}
+                />
+                {!isError ? (
+                <FormHelperText>
+                  Enter the email you'd like to receive the newsletter on.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>can't be empty</FormErrorMessage>
+              )}
+              </div>
+              <div>
+                <FormLabel>Last Name</FormLabel>
+                <Input
+                  type="email"
+                  value={input}
+                  onChange={handleInputChange}
+                />
+                {/* {!isError ? (
+                  <FormHelperText>
+                    Enter the email you'd like to receive the newsletter on.
+                  </FormHelperText>
+                ) : (
+                  <FormErrorMessage>can't be empty</FormErrorMessage>
+                )} */}
+              </div>
+            </div>
+            <div className="address">
+              <FormLabel>Address Line 1</FormLabel>
+              <Input type="email" value={input} onChange={handleInputChange} />
+              {!isError ? (
+                <FormHelperText>
+                  Enter the email you'd like to receive the newsletter on.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>can't be empty</FormErrorMessage>
+              )}
+            </div>
+            <div className="address">
+              <FormLabel>Address Line 2(optional)</FormLabel>
+              <Input type="email" value={input} onChange={handleInputChange} />
+              {/* {!isError ? (
+                  // <FormHelperText>
+                  //   Enter the email you'd like to receive the newsletter on.
+                  // </FormHelperText>
+                ) : (
+                  // <FormErrorMessage>can't be empty</FormErrorMessage>
+                )} */}
+            </div>
+            <div className="Zip">
+              <div>
+                <FormLabel>City</FormLabel>
+                <Input placeholder="First name" />
+              </div>
+
+              <div>
+                <FormLabel>State</FormLabel>
+                <Select placeholder="Select State">
+                  <option>NewYork</option>
+                  <option>Callifornia</option>
+                  <option>Albama</option>
+                  <option>Los Vegas</option>
+                </Select>
+              </div>
+              <div>
+                <FormLabel>Zip</FormLabel>
+                <Input placeholder="First name" />
+              </div>
+            </div>
+            <div className="address">
+              <FormLabel>Phone</FormLabel>
+              {/* <InputLeftAddon children='+91' /> */}
+              <Input
+                type="tel"
+                min={10}
+                value={input}
+                onChange={handleInputChange}
+              />
+              {!isError ? (
+                <FormHelperText>
+                  phone should you'd like to receive the newsletter on.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  Is not a valid phone number. Please enter a 10-digit phone
+                  number.
+                </FormErrorMessage>
+              )}
+            </div>
+            <div id="checkbox">
+              <Stack spacing={5} direction="row">
+                <Checkbox colorScheme="green" defaultChecked>
+                  Receive SMS text message updates on my orders
+                </Checkbox>
+              </Stack>
+            </div>
+            <div className="address">
+              <FormLabel>
+                Email (We require your email to send you receipts and order
+                updates)
+              </FormLabel>
+              <Input type="email" value={input} onChange={handleInputChange} />
+              {!isError ? (
+                <FormHelperText>
+                  Enter the email you'd like to receive the newsletter on.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>can't be empty</FormErrorMessage>
+              )}
+            </div>
+          </FormControl>
+        </div>
+        <div className="Div2">
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            vel minima voluptate dicta quibusdam doloremque temporibus soluta
+            quaerat possimus sint molestias earum reiciendis nihil ab at,
+            praesentium, ipsum autem rerum?
+          </h2>
+        </div>
       </div>
     </div>
   );
